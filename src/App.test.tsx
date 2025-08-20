7f346cef1d7ccc767013ac56f5e0ddd5156e0483
Rebecka94 <rebeckalarsson.1994@gmail.com>
2025-08-20 14:53:00 +0200

    added test to see if button changes theme when clicked

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { expect, test } from "vitest";
 import App from "./App";
 
@@ -6,4 +6,18 @@ test("Theme Switcher text should be visible", () => {
   render(<App />);
   const heading = screen.getByText("Theme Switcher");
   expect(heading).toBeVisible();
+});
+
+test("clicking Light and Dark Mode buttons should change themes", () => {
+  render(<App />);
+
+  const container = screen.getByText("Theme Switcher").parentElement;
+
+  expect(container).toHaveClass("bg-white");
+
+  fireEvent.click(screen.getByText("Dark Mode"));
+  expect(container).toHaveClass("bg-gray-900");
+
+  fireEvent.click(screen.getByText("Light Mode"));
+  expect(container).toHaveClass("bg-white");
 });

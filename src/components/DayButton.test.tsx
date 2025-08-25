d3bf5e90f3db2e48dc02f027494c827d8e0a40ce
Rebecka94 <rebeckalarsson.1994@gmail.com>
2025-08-25 16:35:01 +0200

    edit

--- src/components/DayButton.test.tsx
+++ src/components/DayButton.test.tsx
@@ -1,15 +1,15 @@
 import { fireEvent, render } from "@testing-library/react";
-import { describe, expect, it, vi } from "vitest";
+import { describe, expect, vi, test } from "vitest";
 import DayButton from "./DayButton";
 
 describe("DayButton", () => {
-  it("should render the button with correct text", () => {
+  test("should render the button with correct text", () => {
     const { getByText } = render(<DayButton onClick={() => {}} />);
     const button = getByText("Day");
     expect(button).toBeInTheDocument();
   });
 
-  it("should call onClick when button is clicked", () => {
+  test("should call onClick when button is clicked", () => {
     const mockOnClick = vi.fn();
     const { getByText } = render(<DayButton onClick={mockOnClick} />);
     const button = getByText("Day");
--- src/components/NightButton.test.tsx
+++ src/components/NightButton.test.tsx
@@ -1,15 +1,15 @@
 import { fireEvent, render } from "@testing-library/react";
-import { describe, expect, it, vi } from "vitest";
+import { describe, expect, test, vi } from "vitest";
 import NightButton from "./NightButton";
 
 describe("NightButton", () => {
-  it("should render the button with correct text", () => {
+  test("should render the button with correct text", () => {
     const { getByText } = render(<NightButton onClick={() => {}} />);
     const button = getByText("Night");
     expect(button).toBeInTheDocument();
   });
 
-  it("should call onClick when button is clicked", () => {
+  test("should call onClick when button is clicked", () => {
     const mockOnClick = vi.fn();
     const { getByText } = render(<NightButton onClick={mockOnClick} />);
     const button = getByText("Night");
